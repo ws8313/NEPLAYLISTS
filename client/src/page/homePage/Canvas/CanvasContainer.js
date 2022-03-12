d4f73a68e5ee77f2a29da0ad5a762966ec8c1191
ws8313 <ws8313@naver.com>
2022-03-12 12:38:29 +0900

    change color

--- client/src/page/homePage/Canvas/CanvasContainer.js
+++ client/src/page/homePage/Canvas/CanvasContainer.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense, useState, useEffect } from "react";
 import styled from "styled-components";
 
 import { Canvas, useLoader } from "@react-three/fiber";
@@ -28,6 +28,53 @@ export default function CanvasContainer({ edit }) {
   const dispatch = useDispatch();
   const save_Position = (positions) => dispatch(savePosition(positions));
   // Drag가 안되서 이용 못하는 중
+
+  const [color, setColor] = useState();
+
+  const {idx, category} = useSelector((state) => {
+    return {
+      idx: state.playlist.nowPlaying,
+      category: state
+    };
+  });
+  console.log(category, idx)
+
+  useEffect(() => {
+    let changeColor = category.playlist.playlist[idx].category
+
+    if (changeColor === "joy") {
+      changeColor = "#FFE178"
+    }
+    else if (changeColor === "trust") {
+      changeColor = "#B0E132"
+    }
+    else if (changeColor === "fear") {
+      changeColor = "#2BB176"
+    }
+    else if (changeColor === "surprise") {
+      changeColor = "#41A0B3"
+    }
+    else if (changeColor === "sadness") {
+      changeColor = "#6692F2"
+    }
+    else if (changeColor === "disgust") {
+      changeColor = "#AB8CF5"
+    }
+    else if (changeColor === "anger") {
+      changeColor = "#D64959"
+    }
+    else if (changeColor === "anticiaption") {
+      changeColor = "#E89958"
+    }
+    else if (changeColor === "love") {
+      changeColor = "#EE00FF"
+    }
+    else {
+      changeColor = "white"
+    }
+    setColor(changeColor)
+    console.log(color)
+  }, [category, idx])
 
   const { onNOff } = useSelector((state) => {
     return {
@@ -61,26 +108,26 @@ export default function CanvasContainer({ edit }) {
           castShadow
         />
 
-        {/* <pointLight
-          color="#0000FF"
+        <pointLight
+          color={color}
           intensity={0.4}
           position={[-70, 100, -23]}
         />
         <rectAreaLight
           width={110}
           height={10}
-          color="#0000FF"
+          color={color}
           intensity={30}
           position={[-88.8, 40, -32]}
           rotation={[0, -1.57, 0]}
-        /> */}
+        />
         <Suspense fallback={null}>
           {/* 기본 방 + 버튼 클릭에 따른 Element들 */}
           {/* <Cinema /> */}
           
           {/* <IsometricRoom2 /> */}
           {/* <HomeCinema /> */}
-          {/* <IsometricRoom/> */}
+          <IsometricRoom/>
           {/* <MagicRoom /> */}
           {onNOff[0] ? <Dog/> : <></>}
           {onNOff[1] ? <Cat /> : <></>}
